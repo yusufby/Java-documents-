7c0e1bd1fddf99a97631b52b39ce63d6d8660970
yusufby <yusufbayram19891989@gmail.com>
2022-11-13 21:26:13 +0000

    array

---
+++ src/day14staticarraysforeachloop/arrayPractice.java
@@ -0,0 +1,67 @@
+package day14staticarraysforeachloop;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class arrayPractice {
+
+    public static void main(String[] args) {
+        int[] arrayName = new int[5];
+        System.out.println(Arrays.toString(arrayName));
+        arrayName[1] = 23;
+        arrayName[2] = 21;
+        arrayName[3] = 2;
+        arrayName[4] = 3;
+        System.out.println(Arrays.toString(arrayName));
+
+        System.out.println(arrayName[2]);
+
+        int firstArray = arrayName[0];
+        int lastArray = arrayName[arrayName.length - 1];
+        System.out.println(firstArray);
+        System.out.println(lastArray + firstArray);
+
+        System.out.println("==========");
+        int sum = 0;
+        for (int i = 0; i < arrayName.length; i++) {
+            sum = sum + arrayName[i];
+
+        }
+        System.out.println(sum);
+        System.out.println("???????????");
+        int sum2 = 0;
+        for (int w : arrayName) {
+            sum2 = sum2 + w;
+        }
+        System.out.println(sum2);
+
+        System.out.println("String ==========");
+        String[] name = new String[4];
+
+        name[0] = "yusuf";
+        name[1] = "fatma";
+        name[2] ="yakup";
+        name[3]="Lina Asel";
+        System.out.println(Arrays.toString(name));
+        Arrays.sort(name);
+        System.out.println(Arrays.toString(name));
+        System.out.println("names one by one ");
+        for (String w:name){
+            System.out.println(w);
+        }
+        System.out.println("5  harften kucuk isimler ");
+        for (String w:name){
+            if (w.length()<6){
+                System.out.println(w);
+            }
+        }
+        System.out.println("fatmadan oncekiler");
+            for (String w:name){
+                if (w.contains("fatma")){
+                    break;
+                }
+                System.out.println(w);
+            }
+
+    }
+}
