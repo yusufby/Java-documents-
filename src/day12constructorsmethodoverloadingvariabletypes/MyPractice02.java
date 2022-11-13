812e2d498b6d7f4b15949543fe24e84c9467bcc9
yusufby <yusufbayram19891989@gmail.com>
2022-11-13 00:57:09 +0000

    overloading

--- src/day12constructorsmethodoverloadingvariabletypes/MyPractice02.java
+++ src/day12constructorsmethodoverloadingvariabletypes/MyPractice02.java
@@ -3,27 +3,21 @@ package day12constructorsmethodoverloadingvariabletypes;
 public class MyPractice02 {
     public static void main(String[] args) {
 
-        System.out.println(add(3, 5));
-        System.out.println(add(4, 7));
-        System.out.println(add(3, 5, 7));
-        System.out.println(add(4, 5, 6, 7));
-        System.out.println(add(5.4, 3.3));
+        System.out.println(practice(5,8));
 
+        System.out.println(practice(5,2,7));
+
+
+        System.out.println(practice(5,8,5,9));
     }
 
-    public static int add(int a, int b) {
-        return a + b;
+    public static int practice(int a, int b){
+        return a+b;
+    }
+    public static int practice(int a, int b, int c){
+        return (a+b)-c;
     }
 
-    public static int add(int a, int b, int c) {
-        return a + b + c;
-    }
-
-    public static int add(int a, int b, int c, int d) {
-        return a + b + c + d;
-    }
-
-    public static double add(double a, double b) {
-        return a + b;
-    }
-}
+    public static int practice(int a, int b, int c, int d){
+        return (a+b)-c+d;
+}}
