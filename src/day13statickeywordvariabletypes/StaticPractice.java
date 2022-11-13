1f02c0e20260dc096215a2ea4583a255ff94cddb
yusufby <yusufbayram19891989@gmail.com>
2022-11-13 21:01:57 +0000

    static block

--- src/day13statickeywordvariabletypes/StaticPractice.java
+++ src/day13statickeywordvariabletypes/StaticPractice.java
@@ -2,14 +2,30 @@ package day13statickeywordvariabletypes;
 
 
 public class StaticPractice {
+
+    public  static int counter=0;
+    public   int counter2=0;
+
+    public StaticPractice (){
+        counter++;
+        counter2++;
+    }
+
+
     public static void main(String[] args) {
-        StaticPractice s4 = new StaticPractice();
-        System.out.println(Static01.sCounter);
+    StaticPractice staticPractice=new StaticPractice();
+        System.out.println(staticPractice.counter2);
+        System.out.println(counter);
+
+
+        add(3,5);
 
 
 
-
-
-
-    }}
+    }
+public  static int add(int a, int b){
+        int c=2;
+        return a+b+c;
+}
+}
 
