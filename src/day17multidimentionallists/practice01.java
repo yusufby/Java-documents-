4f6149c110c78a79fb62c791d441cf832a5ba7a6
yusufby <yusufbayram19891989@gmail.com>
2022-11-21 13:19:27 +0000

    list

--- src/day17multidimentionallists/practice01.java
+++ src/day17multidimentionallists/practice01.java
@@ -7,26 +7,33 @@ import java.util.List;
 public class practice01 {
     public static void main(String[] args) {
 
-        List<Integer> l1 = new ArrayList<>();
-        l1.add(34);
-        l1.add(56);
-        l1.add(21);
-        System.out.println(l1);
-        ArrayList<Integer> l2 = new ArrayList<>();
-        l2.add(23);
-        l2.add(9);
-        l2.add(45);
-        l2.add(78);
-        System.out.println(l2);
-        l2.remove(0);
-        System.out.println(l2);
-        Integer sil=Integer.valueOf(45);
-        l2.remove(sil);
-        System.out.println("-------");
-        System.out.println(l2);
-        System.out.println(l1);
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Yusuf");
+        list.add("Yakup");
+        list.add("Fatma");
+        list.add("Lina Asel");
+        list.add("Salih ");
+        list.add("baba");
+        System.out.println(list);
+        System.out.println("yusuf bayram");
+        System.out.println("-----TATLIS------");
 
-        System.out.println(l2.addAll(l1));
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("Yusuf");
+        list1.add("Yakup");
+        list1.add("Fatma");
+        list1.add("Lina Asel");
+        list1.add("Salih ");
+        System.out.println(list1);
+        list1.addAll(2,list);
+        System.out.println(list1);
+        System.out.println("&&&&&&&&&&&7");
+        int i=1;
+        String yakup="one person";
+        System.out.println(yakup);
+        System.out.println(i);
+
+
 
     }
 }
