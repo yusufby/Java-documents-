4f692c05ce038af3cc78c5ba1b1ef79ad373ea7b
yusufby <yusufbayram19891989@gmail.com>
2022-12-13 22:32:25 +0000

    interface

---
+++ src/day29interfacemanipulations/A.java
@@ -0,0 +1,21 @@
+package day29interfacemanipulations;
+
+public interface A {
+    void a();//bydefault, public and abstract
+    void b();
+    void c();
+    void d();
+}
+
+//Creating abstract class that provides the implementation of one method of A interface
+abstract class B implements A{
+    public void c(){System.out.println("I am C");}
+}
+
+//Creating subclass of abstract class, now we need to provide the implementation of rest of the methods
+class M extends B{
+    public void a(){System.out.println("I am a");}
+    public void b(){System.out.println("I am b");}
+    public void d(){System.out.println("I am d");}
+}
+
